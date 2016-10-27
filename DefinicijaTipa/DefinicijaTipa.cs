5356d84142f8c679e9282305fa1388b450c4f14f
Danijel Pavić <danijelpavic21@gmail.com>
2016-10-27 19:59:46 +0200

    Definicija tipa.

--- DefinicijaTipa/DefinicijaTipa.cs
+++ DefinicijaTipa/DefinicijaTipa.cs
@@ -4,26 +4,27 @@ namespace Vsite.CSharp
 {
     class MojaKlasa
     {
-        // TODO: Dodati privatno polje (podatkovni član) "broj" tipa int i dodijeliti mu vrijednost 5
+        int broj = 3;
 
-
-        // TODO: U metodu IspišiČlan dodati naredbu koja će ispisati podatkovni član "broj".
         public void IspišiČlan()
         {
-
+            Console.WriteLine(broj);
         }
     }
 
 
     struct MojaStruktura
     {
-        // TODO: Dodati privatno polje (podatkovni član) "tekst" tipa string i dodijeliti mu vrijednost "MojaStruktura"
+        private string tekst;
 
+        public MojaStruktura(string tekst)
+        {
+            this.tekst = "Moja struktura";
+        }
 
-        // TODO: U metodu IspišiČlan dodati naredbu koja će ispisati podatkovni član "tekst".
         public void IspišiČlan()
         {
-
+            Console.WriteLine(this.tekst);
         }
     }
 
@@ -32,14 +33,14 @@ namespace Vsite.CSharp
     {
         public static void IspišiČlanKlase()
         {
-            // TODO: Inicijalizirati objekt tipa MojaKlasa i pozvati njegovu metodu IspišiČlan
-
+            MojaKlasa mk = new MojaKlasa();
+            mk.IspišiČlan();
         }
 
         public static void IspišiČlanStrukture()
         {
-            // TODO: Inicijalizirati objekt tipa MojaStruktura i pozvati njegovu metodu IspišiČlan koja treba ispisati "MojaStruktura".
-
+            MojaStruktura ms = new MojaStruktura();
+            ms.IspišiČlan();
         }
 
         static void Main(string[] args)
